ac2202c7521ebce1c3bdf3fa1db4e8a0cc600810
Nathan <nathancbrady04@gmail.com>
2025-10-08 20:48:47 -0600

    created the router and the routes, working

--- src/app.jsx
+++ src/app.jsx
@@ -1,49 +1,79 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Assignments } from './Assignments/Assignments';
+import { Goals } from './Goals/Goals';
+import { GospelPlan } from './GospelPlan/GospelPlan';
+import { Home } from './Home/Home';
+import { Login } from './Login/Login';
 
 export default function App() {
     return (
-        <body className="d-flex flex-column min-vh-100">
-            <header>
-                <h1 className="mainName">Procrastinot!</h1>
+        <BrowserRouter>
+            <body className="d-flex flex-column min-vh-100">
+                <header>
+                    <h1 className="mainName">Procrastinot!</h1>
 
-                {/* Main Menu */}
-                <ul className="nav nav-tabs">
-                    <li className="nav-item">
-                        <a className="nav-link active" href="index.html">Login</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="Home.html">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="Assignments.html">Assignments</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="Goals.html">Goals</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="GospelPlan.html">Gospel Study Plan</a>
-                    </li>
+                    {/* Main Menu */}
+                    <ul className="nav nav-tabs">
+                        <li className="nav-item">
+                            <NavLink className='nav-link' to="">
+                                Login
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="Home">
+                                Home
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="Assignments">
+                                Assignments
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="Goals">
+                                Goals
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="GospelPlan">
+                                Gospel Study Plan
+                            </NavLink>
+                        </li>
 
-                </ul>
-            </header>
+                    </ul>
+                </header>
 
-            <main>App components go here</main>
+                <Routes>
+                    <Route path='/' element={<Login />} exact />
+                    <Route path='/Home' element={<Home />} />
+                    <Route path='/Assignments' element={<Assignments />} />
+                    <Route path='/Goals' element={<Goals />} />
+                    <Route path='/GospelPlan' element={<GospelPlan />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
 
-            {/* Footer with Startup Repo */}
-            <footer className="text-dark mt-auto py-4">
-                <div className="container">
-                    <div className="row">
+                {/* Footer with Startup Repo */}
+                <footer className="text-dark mt-auto py-4">
+                    <div className="container">
+                        <div className="row">
 
-                        {/* Left side */}
-                        <div className="col-md-6 mb-3 mb-md-0">
-                            <h5>Startup Repository (Nathan Brady)</h5>
-                            <a className="mb-0" href="https://github.com/Nate-Dawg04/startup.git">Source (Github)</a>
+                            {/* Left side */}
+                            <div className="col-md-6 mb-3 mb-md-0">
+                                <h5>Startup Repository (Nathan Brady)</h5>
+                                <a className="mb-0" href="https://github.com/Nate-Dawg04/startup.git">Source (Github)</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </footer>
-        </body>
+                </footer>
+            </body>
+        </BrowserRouter>
     );
+}
+
+// Handles unknown paths with the router
+function NotFound() {
+    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
